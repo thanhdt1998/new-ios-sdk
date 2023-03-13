40dda131510fc7fd846a23ee27d6179fc311fad9
dang trong thanh <thanhdt@vietmap.vn>
2023-03-13 17:20:09 +0700

    init2

---
+++ Carthage/Checkouts/new-ios-sdk/Package.swift
@@ -0,0 +1,18 @@
+
+import PackageDescription
+
+let package = Package(
+    name: "Map GL Native",
+    products: [
+        .library(
+            name: "Mapbox",
+            targets: ["Mapbox"])
+    ],
+    dependencies: [
+    ],    
+    targets: [
+        .binaryTarget(
+            name: "Mapbox",
+            path: "Mapbox.framework")
+    ]
+)
